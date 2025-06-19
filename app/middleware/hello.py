fedfd189a0e8a9702a590f9b422635ce61c538cf
Alexander Vasiliev <asv23@live.ru>
2025-06-19 14:56:09 +0300

    Test commit

--- app/middleware/hello.py
+++ app/middleware/hello.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
-
+# Some test text
 async def hello_middleware(request: Request, call_next):
     if request.url.path == "/hello":
         return await call_next(request)
